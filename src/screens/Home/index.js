bbb475844ce241b6f651f0c48a8494f7a959a242
MateusRCz <mateusczeszynski@gmail.com>
2025-12-13 12:38:36 -0300

    feat: tela de relatórios e clientes

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -2,8 +2,22 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function Home() {
   return (
-    <View style={{marginTop: 400, justifyContent: 'center', alignItems: 'center'}}>
-          <Text>Lista de relatórios</Text>
+    <View style={{marginTop: 40, justifyContent: 'center', alignItems: 'center'}}>
+          <Text style={{backgroundColor: '#9C9C9C', width: '100%', textAlign: 'center', marginTop: 5, marginBottom: 10, color: '#ffff'}}>Resumo rápido</Text>
+          <View style={{justifyContent: 'center', alignItems: 'center', borderWidth: 1, borderColor: '#D9D9D9', width: 342, height: 280, borderRadius: 10}}>
+              <Text>Gráfico</Text>
+          </View>
+
+          <View style={{flexDirection:'row' , gap: 2, justifyContent: 'center', alignItems: 'center', width: 342, height: 170, borderRadius: 10}}>
+            <View style={{borderWidth: 1, borderColor: '#D9D9D9', width: 113, height: 109, borderRadius: 10, justifyContent: 'center', alignItems: 'center'}}><Text>Todos</Text><Text>1</Text></View>
+            <View style={{borderWidth: 1, borderColor: '#D9D9D9', width: 113, height: 109, borderRadius: 10, justifyContent: 'center', alignItems: 'center'}}><Text>Concluídos</Text><Text>1</Text></View>
+            <View style={{borderWidth: 1, borderColor: '#D9D9D9', width: 113, height: 109, borderRadius: 10, justifyContent: 'center', alignItems: 'center'}}><Text>Pendentes</Text><Text>1</Text></View>
+          </View>
+
+          <View style={{justifyContent: 'center', alignItems: 'center', borderWidth: 1, borderColor: '#D9D9D9', width: 342, height: 270, borderRadius: 10}}>
+              <Text>Gráfico</Text>
+          </View>
+          
     </View>
   );
 }
--- src/screens/customers/index.js
+++ src/screens/customers/index.js
@@ -1,11 +1,35 @@
 // src/screens/customers.tsx
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Button } from 'react-native';
+import { Feather } from '@expo/vector-icons';
 
 export default function Customers() {
   return (
-    <View style={{marginTop: 400, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>Lista de clientes</Text>
+    <View style={{gap: 10, marginTop: 40, justifyContent: 'center', alignItems: 'center'}}>
+          <Text style={{backgroundColor: '#9C9C9C', width: '100%', textAlign: 'center', marginTop: 5, marginBottom: 10, color: '#ffff'}}>Clientes</Text>
+          
+          <Text style={{color: '#BCBCBC'}}>2 encontrados</Text>
+
+          <View style={{flexDirection: 'row', gap: 30, justifyContent: 'center', alignItems: 'center', borderWidth: 1, borderColor: '#D9D9D9', width: 360, height: 65, borderRadius: 10}}>
+              
+              <Feather name="user" size={25} />
+              <Text style={{fontSize: 15}}>Nome</Text>
+              <Text style={{fontSize: 15}}>0000-0000</Text>
+              <Feather name="phone" size={25} />
+              <Feather name="edit" size={25} />
+
+          </View>
+
+          <View style={{flexDirection: 'row', gap: 30, justifyContent: 'center', alignItems: 'center', borderWidth: 1, borderColor: '#D9D9D9', width: 360, height: 65, borderRadius: 10}}>
+              
+              <Feather name="user" size={25} />
+              <Text style={{fontSize: 15}}>Nome</Text>
+              <Text style={{fontSize: 15}}>0000-0000</Text>
+              <Feather name="phone" size={25} />
+              <Feather name="edit" size={25} />
+
+          </View>
+
     </View>
   );
 }
